adf5550f4a6cd39f0cf084878493b2fd97497e71
Margus Lamp <margus.lamp+github@gmail.com>
2017-03-23 17:13:04 +0200

    Some formatting, some fields

--- moviedb_structs.go
+++ moviedb_structs.go
@@ -1,62 +1,63 @@
 package moviedb_structs
 
 import (
-	"strconv"
 	"encoding/json"
 	"regexp"
-	"strings"
 	"sort"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type RottenMoviesResponse struct {
-	Total  int `json:"total"`
+	Total  int           `json:"total"`
 	Movies []RottenMovie `json:"movies"`
 }
 
 type RottenTvResponse struct {
-	PageCount  int `json:"pageCount"`
-	TotalCount int `json:"totalCount"`
+	PageCount  int        `json:"pageCount"`
+	TotalCount int        `json:"totalCount"`
 	TvSeries   []RottenTv `json:"tvSeries"`
 }
 
 type RottenTv struct {
-	Title       string `json:"title"`
+	Title       string      `json:"title"`
 	EndYear     json.Number `json:"endYear,omitempty"`
 	StartYear   json.Number `json:"startYear"`
-	PosterImage string `json:"posterImage,omitempty"`
-	MeterClass  string `json:"meterClass"`
-	Image       string `json:"image"`
-	URL         string `json:"url"`
+	PosterImage string      `json:"posterImage,omitempty"`
+	MeterClass  string      `json:"meterClass"`
+	Image       string      `json:"image"`
+	URL         string      `json:"url"`
 	MeterValue  json.Number `json:"meterValue,omitempty"`
 }
 
 type RottenMovie struct {
-	ID               string `json:"id"`
-	Title            string `json:"title"`
+	ID               string      `json:"id"`
+	Title            string      `json:"title"`
 	Year             json.Number `json:"year"`
-	MpaaRating       string `json:"mpaa_rating"`
+	MpaaRating       string      `json:"mpaa_rating"`
 	Runtime          json.Number `json:"runtime"`
-	CriticsConsensus string `json:"critics_consensus,omitempty"`
-	ReleaseDates struct {
+	CriticsConsensus string      `json:"critics_consensus,omitempty"`
+	ReleaseDates     struct {
 		Theater string `json:"theater"`
 		Dvd     string `json:"dvd"`
 	} `json:"release_dates"`
 	Ratings struct {
-		CriticsRating  string `json:"critics_rating"`
+		CriticsRating  string      `json:"critics_rating"`
 		CriticsScore   json.Number `json:"critics_score"`
-		AudienceRating string `json:"audience_rating"`
+		AudienceRating string      `json:"audience_rating"`
 		AudienceScore  json.Number `json:"audience_score"`
 	} `json:"ratings"`
 	Synopsis string `json:"synopsis"`
-	Posters struct {
+	Posters  struct {
 		Thumbnail string `json:"thumbnail"`
 		Profile   string `json:"profile"`
 		Detailed  string `json:"detailed"`
 		Original  string `json:"original"`
 	} `json:"posters"`
 	AbridgedCast []struct {
-		Name       string `json:"name"`
-		ID         string `json:"id"`
+		Name       string   `json:"name"`
+		ID         string   `json:"id"`
 		Characters []string `json:"characters"`
 	} `json:"abridged_cast"`
 	Links struct {
@@ -69,27 +70,28 @@ type RottenMovie struct {
 }
 
 type OMDbSingleResult struct {
-	Title        string `json:"Title"`
-	Year         string `json:"Year"`
-	Rated        string `json:"Rated"`
-	Released     string `json:"Released"`
-	Runtime      string `json:"Runtime"`
-	Genre        string `json:"Genre"`
-	Director     string `json:"Director"`
-	Writer       string `json:"Writer"`
-	Actors       string `json:"Actors"`
-	Plot         string `json:"Plot"`
-	Language     string `json:"Language"`
-	Country      string `json:"Country"`
-	Awards       string `json:"Awards"`
-	Poster       string `json:"Poster"`
-	Metascore    string `json:"Metascore"`
-	ImdbRating   string `json:"imdbRating"`
-	ImdbVotes    string `json:"imdbVotes"`
-	ImdbID       string `json:"imdbID"`
-	Type         string `json:"Type"`
-	TotalSeasons string `json:"totalSeasons"`
-	Response     string `json:"Response"`
+	Title        string    `json:"Title"`
+	Year         string    `json:"Year"`
+	Rated        string    `json:"Rated"`
+	Released     string    `json:"Released"`
+	Runtime      string    `json:"Runtime"`
+	Genre        string    `json:"Genre"`
+	Director     string    `json:"Director"`
+	Writer       string    `json:"Writer"`
+	Actors       string    `json:"Actors"`
+	Plot         string    `json:"Plot"`
+	Language     string    `json:"Language"`
+	Country      string    `json:"Country"`
+	Awards       string    `json:"Awards"`
+	Poster       string    `json:"Poster"`
+	Metascore    string    `json:"Metascore"`
+	ImdbRating   string    `json:"imdbRating"`
+	ImdbVotes    string    `json:"imdbVotes"`
+	ImdbID       string    `json:"imdbID"`
+	Type         string    `json:"Type"`
+	TotalSeasons string    `json:"totalSeasons"`
+	Response     string    `json:"Response"`
+	LastUpdated  time.Time `json:"-"`
 }
 
 type MovieRottenScore struct {
@@ -98,13 +100,15 @@ type MovieRottenScore struct {
 }
 
 type Movie struct {
-	Title        string `json:"title"`
-	Year         int `json:"year"`
-	RottenLink   string `json:"rottenLink"`
+	ID           string           `json:"id"`
+	Title        string           `json:"title"`
+	Year         int              `json:"year"`
+	RottenLink   string           `json:"rottenLink"`
 	RottenScores MovieRottenScore `json:"rottenScores"`
-	ImdbId       string `json:"imdbId"`
-	Actors       []Actor `json:"actors"`
-	MatchScore   int `json:"matchScore"`
+	ImdbId       string           `json:"imdbId"`
+	Actors       []Actor          `json:"actors"`
+	MatchScore   int              `json:"matchScore"`
+	LastUpdated  time.Time        `json:"-"`
 }
 
 type Actor struct {
@@ -112,17 +116,21 @@ type Actor struct {
 }
 
 type Tv struct {
-	Title        string `json:"title"`
-	YearFrom     int `json:"yearFrom"`
-	YearTo       int `json:"yearTo"`
+	ID           string        `json:"id"`
+	Title        string        `json:"title"`
+	YearFrom     int           `json:"yearFrom"`
+	YearTo       int           `json:"yearTo"`
 	RottenScores TvRottenScore `json:"rottenScores"`
-	RottenLink   string `json:"rottenLink"`
-	ImdbId       string `json:"imdbId"`
+	RottenLink   string        `json:"rottenLink"`
+	ImdbId       string        `json:"imdbId"`
+	Actors       []Actor       `json:"actors"`
+	MatchScore   int           `json:"matchScore"`
+	LastUpdated  time.Time     `json:"-"`
 }
 
 type TvRottenScore struct {
-	CriticsScore  int `json:"criticsScore"`
-	AudienceScore int `json:"audienceScore"`
+	CriticsScore  json.Number `json:"criticsScore"`
+	AudienceScore json.Number `json:"audienceScore"`
 }
 
 func MovieFromOmdbSingleResult(omdbMovie OMDbSingleResult) (movie Movie) {
@@ -157,7 +165,7 @@ func TvFromRottenTv(rottenTv RottenTv) (tv Tv) {
 	tv.Title = rottenTv.Title
 	tv.YearFrom, _ = strconv.Atoi(string(rottenTv.StartYear))
 	tv.YearTo, _ = strconv.Atoi(string(rottenTv.EndYear))
-	tv.RottenScores.CriticsScore, _ = strconv.Atoi(string(rottenTv.MeterValue))
+	tv.RottenScores.CriticsScore = json.Number(rottenTv.MeterValue)
 	// Cleanup url
 	re := regexp.MustCompile("^(/tv/[a-z_-]{1,25})/?")
 	matches := re.FindStringSubmatch(rottenTv.URL)
@@ -173,21 +181,33 @@ func TvFromOmdbSingleResult(omdbResult OMDbSingleResult) (tv Tv) {
 	re := regexp.MustCompile(`(\d*)–(\d*)`)
 	yearMatches := re.FindStringSubmatch(omdbResult.Year)
 	if len(yearMatches) > 1 {
-		tv.YearFrom, _ = strconv.Atoi(yearMatches[1]);
-		tv.YearTo, _ = strconv.Atoi(yearMatches[2]);
+		tv.YearFrom, _ = strconv.Atoi(yearMatches[1])
+		tv.YearTo, _ = strconv.Atoi(yearMatches[2])
 	}
 	tv.ImdbId = omdbResult.ImdbID
 	return tv
 }
 
 // MatchScoreSorter sorts movies by matchScore.
-type MatchScoreSorter []Movie
+type MovieMatchScoreSorter []Movie
 
-func (a MatchScoreSorter) Len() int           { return len(a) }
-func (a MatchScoreSorter) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a MatchScoreSorter) Less(i, j int) bool { return a[i].MatchScore > a[j].MatchScore }
+func (a MovieMatchScoreSorter) Len() int           { return len(a) }
+func (a MovieMatchScoreSorter) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a MovieMatchScoreSorter) Less(i, j int) bool { return a[i].MatchScore > a[j].MatchScore }
 
-func SortMoviesByMatchScore(movies []Movie) ([]Movie) {
-	sort.Sort(MatchScoreSorter(movies))
+func SortMoviesByMatchScore(movies []Movie) []Movie {
+	sort.Sort(MovieMatchScoreSorter(movies))
 	return movies
+}
+
+// MatchScoreSorter sorts movies by matchScore.
+type TvMatchScoreSorter []Tv
+
+func (a TvMatchScoreSorter) Len() int           { return len(a) }
+func (a TvMatchScoreSorter) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a TvMatchScoreSorter) Less(i, j int) bool { return a[i].MatchScore > a[j].MatchScore }
+
+func SortTvsByMatchScore(tvs []Tv) []Tv {
+	sort.Sort(TvMatchScoreSorter(tvs))
+	return tvs
 }
